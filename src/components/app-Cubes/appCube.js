1d546c962ee26ff47faeab3a05e62488af241300
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2023-03-01 18:56:01 -0500

    Created Custom Element for App Cubes

--- src/components/app-Cubes/appCube.js
+++ src/components/app-Cubes/appCube.js
@@ -14,25 +14,56 @@ class appCubes extends HTMLElement {
         this[propName] = newValue;
         this.render();
     }
-    render(){
 
+    render() {
+
+        // Crea un arreglo de imagenes
         const images = [
-            "../../../dist/.pngs/deepl.png",
-            "../../../dist/.pngs/deepl.png",
-            "../../../dist/.pngs/deepl.png"
-        ]
-
-
-        const misMarcadores = document.getElementById("marcadores")
-
+          "../../../dist/.pngs/deepl.png",
+          "../../../dist/.pngs/deepl.png",
+          "../../../dist/.pngs/deepl.png",
+          "../../../dist/.pngs/gitHub.png"
+        ];
         
+        //Crea mi variable que almacenará la injeccion de cada componente "marcaador"
+        let htmlContenido = "";
 
-        for (let index = 0; index < images.length; index++) {
-            const element = array[index];
+        //Se hace un for donde añado el index de mi imagen a el src de cada "marcador"
+        for (let i = 0; i < images.length; i++) {
+          htmlContenido += `
+            <section id="marcador" class="marcador">
+              <img src="${images[i]}" alt="">
+            </section>
+          `;
         }
-
-        this.shadowRoot.innerHTML = `<h1> FireFox </h1> ${""}`
-    }
+        
+        //Añado estilos porque no los coje del link de mi html
+        this.shadowRoot.innerHTML = `
+        <style> .marcadores{
+            margin-top: 100px;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+        }
+        
+        .marcador{
+            display: flex;
+            width: 150px;
+            height: 150px;
+            box-shadow: 5px 5px 5px rgb(193 193 193);
+            justify-content: center;
+        }
+        
+        .marcador img {
+            width: 100px;
+            object-fit: contain;
+        }
+        </style>
+        <div id="marcadores" class="marcadores">
+        ${ //Añado mi variable que contiene cada uno de los componentes previo
+            htmlContenido}
+    </div>`;
+      }
 }
 
 customElements.define("app-cubes", appCubes);
