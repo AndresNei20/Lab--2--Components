804e8c4fc071c955f146d72859de784f1414c40c
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2023-03-08 18:01:56 -0500

    changed the component for app cubes

--- src/components/app-Cubes/appCube.js
+++ src/components/app-Cubes/appCube.js
@@ -1,13 +1,16 @@
+import data from "./data.js"
+
+
 class appCubes extends HTMLElement {
-    static get observedAttributes(){
-        return[];
+    static get observedAttributes() {
+        return [];
     }
-    constructor(){
+    constructor() {
         super();
         console.log("traido")
-        this.attachShadow({mode: "open"})
+        this.attachShadow({ mode: "open" })
     }
-    connectedCallback(){
+    connectedCallback() {
         this.render();
     }
     attributeChangedCallback(propName, oldValue, newValue) {
@@ -15,34 +18,38 @@ class appCubes extends HTMLElement {
         this.render();
     }
 
-    render() {
-      
-      
-       const images = [
-          "../../../dist/.pngs/deepl.png",
-          "../../../dist/.pngs/deepl.png",
-          "../../../dist/.pngs/deepl.png",
-          "../../../dist/.pngs/gitHub.png"
-        ];
-        
-    
-        let htmlContenido = "";
 
-        //Se hace un for donde añado el index de mi imagen a el src de cada "marcador"
-        for (let i = 0; i < images.length; i++) {
-          htmlContenido += `
+
+    render() {
+        data.forEach((data) => {
+            this.shadowRoot.innerHTML += `
+            <link rel="stylesheet" href="../src/components/app-Cubes/appCube.css">
             <section id="marcador" class="marcador">
-              <img src="${images[i]}" alt="">
+             <img src="${data.thumbnail}" alt="">
+             <h6>${data.name}</h6>
             </section>
-          `;
-        } 
-        //Añado estilos porque no los coje del link de mi html
-        this.shadowRoot.innerHTML = `
-        <link rel="stylesheet" href="../src/components/app-Cubes/appCube.css">">
-        <div id="marcadores" class="marcadores">
-        ${htmlContenido}
-    </div>`; 
-      }
+
+
+    `
+
+        })
+
+
+
+    }
+
+    //   for (let i = 0; i < images.length; i++) {
+    //     htmlContenido += `
+    //       <section id="marcador" class="marcador">
+    //         <img src="${images[i]}" alt="">
+    //       </section>
+    //     `;
+    //   } 
+    //   this.shadowRoot.innerHTML = `
+    //   
+    //   <div id="marcadores" class="marcadores">
+    //   ${htmlContenido}; 
+
 }
 
 customElements.define("app-cubes", appCubes);
---
+++ src/components/app-Cubes/data.js
@@ -0,0 +1,58 @@
+export default [
+    {
+      name: "Git Hub",
+      thumbnail:
+        "https://as01.epimg.net/meristation/imagenes/2021/10/25/noticias/1635151110_716951_1635151285_noticia_normal.jpg",
+      
+    },
+    {
+        name: "DeepL",
+        thumbnail:
+          "https://as01.epimg.net/meristation/imagenes/2021/10/25/noticias/1635151110_716951_1635151285_noticia_normal.jpg",
+        
+      },
+      
+      {
+        name: "Google Drive",
+        thumbnail:
+          "https://as01.epimg.net/meristation/imagenes/2021/10/25/noticias/1635151110_716951_1635151285_noticia_normal.jpg",
+        
+      },
+      
+      {
+        name: "Youtube",
+        thumbnail:
+          "https://as01.epimg.net/meristation/imagenes/2021/10/25/noticias/1635151110_716951_1635151285_noticia_normal.jpg",
+        
+      },
+      
+      {
+        name: "Netflix",
+        thumbnail:
+          "https://as01.epimg.net/meristation/imagenes/2021/10/25/noticias/1635151110_716951_1635151285_noticia_normal.jpg",
+        
+      },
+      
+      {
+        name: "Miro",
+        thumbnail:
+          "https://as01.epimg.net/meristation/imagenes/2021/10/25/noticias/1635151110_716951_1635151285_noticia_normal.jpg",
+        
+      },
+      
+      {
+        name: "nodeJS",
+        thumbnail:
+          "https://as01.epimg.net/meristation/imagenes/2021/10/25/noticias/1635151110_716951_1635151285_noticia_normal.jpg",
+        
+      },
+      
+      {
+        name: "Hackerrun",
+        thumbnail:
+          "https://as01.epimg.net/meristation/imagenes/2021/10/25/noticias/1635151110_716951_1635151285_noticia_normal.jpg",
+        
+      },
+    
+  ];
+  
